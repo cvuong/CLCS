7291e226faf40dc368596eb2ae45b72d0dee5bb7
Clayton Mellina <cemellina@gmail.com>
2012-06-01 18:58:17 -0700

    trying to debug

--- clcs-fast.cpp
+++ clcs-fast.cpp
@@ -10,8 +10,8 @@ int arr[MAX_SIZE][MAX_SIZE];
 char walk[MAX_SIZE][MAX_SIZE];
 
 int m, n;
-int upper_bounds[MAX_SIZE][MAX_SIZE];
 int lower_bounds[MAX_SIZE][MAX_SIZE];
+int upper_bounds[MAX_SIZE][MAX_SIZE];
 int path_lengths[MAX_SIZE];
 
 #define DOWN 'a'
@@ -65,30 +65,33 @@ int LCS() {
   return arr[m][n];
 }
 
-void singleShortestPath(int mid, int low, int up) {
+void fillDP(int mid, int low, int up) {
 	//int m = 2*A.length(), n = B.length();
   int i, j;
 	int start_col, end_col;
 
-  for (i = mid; i < up + m; i++) {	// valid rows to look in 
+  for (i = m; i <= up + m; i++) {	// valid rows to look in 
 		
 		if (i < up) {
 			start_col = 1;
 		} else {
-			start_col = upper_bounds[up][i - up] + 1;
+			start_col = max(upper_bounds[up][i - up],1);
 		}
 		
+		/*
 		if (i > low + m) {
-			end_col = n + 1;
+			end_col = n;
 		} else {
-			end_col = lower_bounds[low][i - low] + 1;
+			end_col = lower_bounds[low][i - low];
 		}
+		*/
 		
+		end_col = n;
 		//cout << " row: " << i << " start: " << start_col << " end: " << end_col << endl;
 
-    for (j = start_col; j < end_col; j++) {
+    for (j = start_col; j <= end_col; j++) {
 	
-			cout << i << " " << j << endl;
+			//cout << i << " " << j << endl;
       arr[i][j] = max(arr[i-1][j], arr[i][j-1]);
       if (doubleA[i-1] == B[j-1]) arr[i][j] = max(arr[i][j], arr[i-1][j-1]+1);
     }
@@ -98,8 +101,8 @@ void singleShortestPath(int mid, int low, int up) {
 }
 
 void initBounds() {
-	upper_bounds[0][m] = n;
 	lower_bounds[0][m] = n;
+	upper_bounds[0][m] = n;
 	
 	int length = 0;
 	
@@ -108,7 +111,7 @@ void initBounds() {
 	while (true) {
 		
 		//cout << "row: " << row << " col: " << col << endl;
-		lower_bounds[0][row] = col;
+		upper_bounds[0][row] = col;
 		
 		if (row == 0 && col == 0) {
 			break;
@@ -121,7 +124,7 @@ void initBounds() {
 			continue;
 		} else if (col == 0) {	// move up
 			row--;
-			upper_bounds[0][row] = col;
+			lower_bounds[0][row] = col;
 			continue;
 		}
 		
@@ -129,35 +132,88 @@ void initBounds() {
 		if (A.at(row-1) == B.at(col-1)) {	
 			//cout << "diag" << endl;
 			row--; col--;
-			upper_bounds[0][row] = col;
+			lower_bounds[0][row] = col;
 		} else if(arr[row-1][col] <= arr[row][col-1]) {	
 			//cout << "left" << endl;
-			//lower_bounds[0][row] = col;
+			//upper_bounds[0][row] = col;
 			col--;
 		} else {
 			//cout << "up" << endl;
 			row--;
-			upper_bounds[0][row] = col;
+			lower_bounds[0][row] = col;
 		}
 	}
 	path_lengths[0] = length;
 	return;
 }
 
+
+void traceBack(int start_row) {
+	lower_bounds[start_row][m] = n;
+	upper_bounds[start_row][m] = n;
+	
+	int length = 0;
+	//cout << "traceback " << start_row << endl;
+	
+	//find lower bound
+	int row = start_row + m, col = n;
+	while (true) {
+		
+		//cout << "row: " << row << " col: " << col << endl;
+		upper_bounds[start_row][row] = col;
+		
+		if (row == start_row && col == 0) {
+			break;
+		}
+		
+		length++;
+		
+		if (row == start_row) {	// move left
+			col--;
+			continue;
+		} else if (col == 0) {	// move up
+			row--;
+			lower_bounds[start_row][row] = col;
+			continue;
+		}
+		
+		if (doubleA.at(row-1) == B.at(col-1)) {	
+			row--; col--;
+			lower_bounds[start_row][row] = col;
+		} else if(arr[row-1][col] <= arr[row][col-1]) {	
+			col--;
+		} else {
+			//cout << "up" << endl;
+			row--;
+			lower_bounds[start_row][row] = col;
+		}
+	}
+	path_lengths[start_row] = length;
+	return;
+}
+
+void findShortestPaths(int low, int up) {
+	if (up - low <= 1) return;
+	int mid = (low + up) / 2;
+	traceBack(mid);
+	findShortestPaths(low, mid);
+	findShortestPaths(mid, up);
+}
+
 void printBounds(int path) {
 	
 	cout << "length: " << path_lengths[path] << endl;
 	
-	cout << "upper bound path: ";
+	cout << "lower bound path: ";
 	for (int i = 0; i <= m; i++) {
-		cout << upper_bounds[path][i] << ' ';
+		cout << lower_bounds[path][i] << ' ';
 	}
 	cout << endl;
 	
 	
-	cout << "lower bound path: ";
+	cout << "upper bound path: ";
 	for (int i = 0; i <= m; i++) {
-		cout << lower_bounds[path][i] << ' ';
+		cout << upper_bounds[path][i] << ' ';
 	}
 	cout << endl;
 	
@@ -190,20 +246,43 @@ int main() {
 		doubleA = A + A;
 		
 		int result = LCS();
-		initBounds();
-		
+		//initBounds();
+		traceBack(0);
 		// shift paths downs
 		for (int i = 0; i <= m; i++) {
-			lower_bounds[m][i] = lower_bounds[0][i];
 			upper_bounds[m][i] = upper_bounds[0][i];
+			lower_bounds[m][i] = lower_bounds[0][i];
 		}
 		path_lengths[m] = path_lengths[0];
 	
 		//printBounds(0);
 		
-		singleShortestPath((m)/2, 0, m);
+		fillDP((m)/2, 0, m);
+		printDPTable();
 		
-		//printDPTable();
+		findShortestPaths(0, m);
+		
+
+		int min_path, min_length = m + n;
+		for (int i = 0; i <= m; i++) {
+			if (path_lengths[i] < min_length) {
+				min_path = i;
+				min_length = path_lengths[i];
+			}
+		}
+		
+		cout << "clcs lengths" << endl;
+		for (int i = 0; i <= m; i++) {
+			cout << path_lengths[i] << " " << arr[i + m][n] << endl;
+			/*
+			if (path_lengths[i] == min_length) {
+				cout << arr[min_path + m][n] << endl;
+			}*/
+		}
+		cout << endl;
+		//cout << arr[min_path + m][n] << endl;
+		//cout << min_path << endl;
+		
 		
 		//
 		
